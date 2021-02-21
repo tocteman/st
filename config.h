8765b78c5330b2e74f25452340be3964420170ed
void <juanfrancisco.vinueza@gmail.com>
2021-02-21 03:48:39 -0500

    dracula colorscheme

--- config.h
+++ config.h
@@ -111,22 +111,22 @@ float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	"#cc241d",
-	"#98971a",
-	"#d79921",
-	"#458588",
-	"#b16286",
-	"#689d6a",
-	"#a89984",
-	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
+	"#141223", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	"#ff5555",
+	"#50fa7b",
+	"#f1fa8c",
+	"#bd93f9",
+	"#ff79c6",
+	"#8be9fd",
+	"#bfbfbf",
+	"#4d4d4d",
+	"#ff6e67",
+	"#5af78e",
+	"#f4f99d",
+	"#caa9fa",
+	"#ff92d0",
+	"#9aedfe",
+	"#e6e6e6",
 	[255] = 0,
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#add8e6", /* 256 -> cursor */
